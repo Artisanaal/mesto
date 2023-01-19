8b02609fc9d9ba4f8299c3a5006d23dd674d9c2a
Artisanaal <103187259+Artisanaal@users.noreply.github.com>
2023-01-19 16:38:00 +0300

    Все ошибки исправлены

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
+const popupOpenButtonElement = document.querySelector('.menu__open-popup');
+console.log(popupCloseButtonElement);
+console.log(popupOpenButtonElement);
+
+const openPopup = function() {
+    popupElement.classList.add('popup__is-opened');
+}
+
+const closePopup = function() {
+    popupElement.classList.remove('popup__is-opened');
+}
+
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+
+
+    let formElement = document.querySelector('.popup');
+
+    let nameInput = document.querySelector('.popup__name');
+    let jobInput = document.querySelector('.popup__info');
+    let titleProfile = document.querySelector('.profile__info-title');
+    let subtitleProfile = document.querySelector('.profile__info-subtitle');
+
+    function formSubmitHandler (evt) {
+        evt.preventDefault();
+
+        titleProfile.textContent = nameInput.value;
+        subtitleProfile.textContent = jobInput.value;
+        closePopup();
+    }
+
+formElement.addEventListener('submit', formSubmitHandler);
